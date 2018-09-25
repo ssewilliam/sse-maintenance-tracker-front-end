5186c91ac25f831a1acfd8fc5023e6a2fec6333d
william sserubiri <william.sserubiri@andela.com>
2018-09-25 18:54:24 +0300

    [Chore #160690273] Setup react router

---
+++ src/containers/Login/Login.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import '../../CSS/style.css';
+import Logo from '../../images/mt-logo.jpg';
+
+class Login extends Component {
+ render () {
+   return(
+    < div className="login_register_wrap">
+        <div className="login_register_top">
+            <div className="login_register_logo">
+                <img src={Logo} className="img_class" alt="maintenace tracker" />
+            </div>
+        </div>
+        <h2 className="title text-center">User Login</h2>
+        <form action="requests.html" autocomplete="off">
+            <div className="form_group">
+                <label for="username"></label>
+                <input type="text" name="username" required id="username" placeholder="username" />
+            </div>
+
+            <div className="form_group">
+                <label for="password"></label>
+                <input type="password" required name="password" id="password" placeholder="password"/>
+            </div>
+
+            <div className="form_group">
+                <input type="submit" value="Login" />
+                <span className="not-a-member">
+                    Not a member, <a href="register.html">register</a>
+                </span>
+                <span className="not-a-member">
+                    OR
+                </span>
+                <span className="not-a-member">
+                    <a href="admin/login.html">Login as an Admin</a>
+                </span>
+            </div>
+        </form>
+    </div>
+   )
+ }
+}
+export default Login;
